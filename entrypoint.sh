723dbb39f14acdf26f7f78e5970dc3030cdc12b5
Qian Linfeng <thewawar@gmail.com>
2018-05-19 00:53:45 +0800

    Fix start container bug

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,7 @@
 # Add local user
 # use USER_ID
 
-useradd --shell /bin/bash -u ${USER_ID} -o -c "" -d /opt -m user
+useradd --shell /bin/bash -u ${USER_ID} -o -c "" -d /opt user
 echo "user ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
+
+exec /usr/bin/gosu user "$@"
